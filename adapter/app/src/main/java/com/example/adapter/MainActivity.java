8d642ac1fc40305a5cd2d4e84032834dca67a76d
Tendo456 <rbgamero456@gmail.com>
2021-01-22 09:30:51 -0500

    actualizacion

--- adapter/app/src/main/java/com/example/adapter/MainActivity.java
+++ adapter/app/src/main/java/com/example/adapter/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.adapter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MainActivity extends AppCompatActivity implements  UsersAdapter.ClickedItem{
+
+    Toolbar toolbar;
+    RecyclerView recyclerView;
+    String n="1";
+    UsersAdapter usersAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        recyclerView = findViewById(R.id.recyclerview);
+
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL));
+
+        usersAdapter = new UsersAdapter(this::ClickedUser);
+
+        getAllUsers();
     }
+
+
+
+    public void getAllUsers(){
+        //Integer.parseInt(n)
+        Call<List<UserResponse>> userlist = ApiClient.getUserService().getPost();
+        userlist.enqueue(new Callback<List<UserResponse>>() {
+            @Override
+            public void onResponse(Call<List<UserResponse>> call, Response<List<UserResponse>> response) {
+             if(response.isSuccessful()){
+                 List<UserResponse> userResponses = response.body();
+                 usersAdapter.setData(userResponses);
+                 recyclerView.setAdapter(usersAdapter);
+             }
+            }
+
+            @Override
+            public void onFailure(Call<List<UserResponse>> call, Throwable t) {
+                Log.e("Failure",t.getLocalizedMessage());
+            }
+        });
+
+    }
+
+
+    @Override
+    public void ClickedUser(UserResponse userResponse) {
+    }
+
+
 }
--- adapter/app/src/main/java/com/example/adapter/UserService.java
+++ adapter/app/src/main/java/com/example/adapter/UserService.java
@@ -1,4 +1,32 @@
 package com.example.adapter;
 
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 public interface UserService {
+
+    @GET("posts")
+    Call<List<UserResponse>> getPost();
+
+    @POST("posts")
+    Call<UserRequest> postDataToServer(@Body UserRequest userRequest);
+
+
+    @GET("posts")
+    Call<List<UserResponse>> getPostById(@Query("userId") Integer userId);
+
+    @PATCH("posts/{id}")
+    Call<List<UserResponse>> getPatch(@Path("id") int id, @Body UserResponse userResponse);
+
+
+    @DELETE("posts/{id}")
+    Call<Void> deletePost(@Path("id") int id);
 }
