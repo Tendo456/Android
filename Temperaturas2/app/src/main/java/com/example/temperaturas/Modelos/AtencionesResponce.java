ff86249b8bae50e120072c03e056bae3173f9a26
Tendo456 <rbgamero456@gmail.com>
2021-01-23 08:27:05 -0500

    1

--- Temperaturas2/app/src/main/java/com/example/temperaturas/Modelos/AtencionesResponce.java
+++ Temperaturas2/app/src/main/java/com/example/temperaturas/Modelos/AtencionesResponce.java
@@ -1,4 +1,110 @@
 package com.example.temperaturas.Modelos;
 
 public class AtencionesResponce {
+    private int idatention;
+    private int idpatient;
+    private double latitud;
+    private double longitud;
+    private int iduser;
+    private String created_at;
+    private String updated_at;
+    private String fecha;
+    private String hora;
+    private String hash;
+
+    public int getIdatention() {
+        return idatention;
+    }
+
+    public void setIdatention(int idatention) {
+        this.idatention = idatention;
+    }
+
+    public int getIdpatient() {
+        return idpatient;
+    }
+
+    public void setIdpatient(int idpatient) {
+        this.idpatient = idpatient;
+    }
+
+    public double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(double latitud) {
+        this.latitud = latitud;
+    }
+
+    public double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(double longitud) {
+        this.longitud = longitud;
+    }
+
+    public int getIduser() {
+        return iduser;
+    }
+
+    public void setIduser(int iduser) {
+        this.iduser = iduser;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(String updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    @Override
+    public String toString() {
+        return "AtencionesResponce{" +
+                "idatention=" + idatention +
+                ", idpatient=" + idpatient +
+                ", latitud=" + latitud +
+                ", longitud=" + longitud +
+                ", iduser=" + iduser +
+                ", created_at='" + created_at + '\'' +
+                ", updated_at='" + updated_at + '\'' +
+                ", fecha='" + fecha + '\'' +
+                ", hora='" + hora + '\'' +
+                ", hash='" + hash + '\'' +
+                '}';
+    }
 }
