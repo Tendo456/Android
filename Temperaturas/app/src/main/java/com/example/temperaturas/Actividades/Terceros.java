93cac3927a28b7f5ba44a0f479cc86e7578c969e
Tendo456 <rbgamero456@gmail.com>
2021-03-16 12:39:26 -0500

    new

--- Temperaturas/app/src/main/java/com/example/temperaturas/Actividades/Terceros.java
+++ Temperaturas/app/src/main/java/com/example/temperaturas/Actividades/Terceros.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
@@ -49,6 +50,11 @@ public class Terceros extends AppCompatActivity {
     );
 
     @Override
+    public void startActivityForResult(Intent intent, int requestCode, @Nullable Bundle options) {
+        super.startActivityForResult(intent, requestCode, options);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_terceros);
@@ -83,6 +89,7 @@ public class Terceros extends AppCompatActivity {
 
         mAuthListener = firebaseAuth -> {
             FirebaseUser user = firebaseAuth.getCurrentUser();
+            user.sendEmailVerification();
             if (user != null) {
 
                 continuar.setEnabled(true);
