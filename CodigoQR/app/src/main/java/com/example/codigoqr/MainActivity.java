49206f308069d2646b294c10bbdab8ebb09b02a8
Tendo456 <rbgamero456@gmail.com>
2021-03-16 07:30:07 -0500

    codigo

--- CodigoQR/app/src/main/java/com/example/codigoqr/MainActivity.java
+++ CodigoQR/app/src/main/java/com/example/codigoqr/MainActivity.java
@@ -60,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
             super.onActivityResult(requestCode, resultCode, data);
         }
 
+        assert result != null;
         String datos = result.getContents();
 
         txtLink.setText(datos);
