12c8a04963fd9e2bb41dc9552d585b50de0f601d
Tendo456 <rbgamero456@gmail.com>
2021-04-27 08:42:39 -0500

    lab

---
+++ LaboratorioMolecular/app/src/main/java/com/example/laboratoriomolecular/Formularios.java
@@ -0,0 +1,14 @@
+package com.example.laboratoriomolecular;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Formularios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formularios);
+    }
+}
