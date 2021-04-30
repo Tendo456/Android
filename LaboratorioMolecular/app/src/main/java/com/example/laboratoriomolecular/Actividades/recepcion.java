beba0131ffefb02887c8e972aba2579f72c4b760
Tendo456 <rbgamero456@gmail.com>
2021-04-30 10:45:21 -0500

    recepcion

--- LaboratorioMolecular/app/src/main/java/com/example/laboratoriomolecular/Actividades/recepcion.java
+++ LaboratorioMolecular/app/src/main/java/com/example/laboratoriomolecular/Actividades/recepcion.java
@@ -2,15 +2,30 @@ package com.example.laboratoriomolecular.Actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.laboratoriomolecular.Add_recepcion;
 import com.example.laboratoriomolecular.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class recepcion extends AppCompatActivity {
+
+    FloatingActionButton addRecepcion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recepcion);
+
+        addRecepcion = findViewById(R.id.addRecepcion);
+
+        addRecepcion.setOnClickListener(v -> AddRecepcion());
+    }
+
+    public void AddRecepcion (){
+
+        startActivity(new Intent(getApplicationContext(), Add_recepcion.class));
     }
 }
