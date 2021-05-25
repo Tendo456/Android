356772b09124cc9fbbe15667b0ba7984827145ff
Tendo456 <rbgamero456@gmail.com>
2021-05-25 13:49:50 -0500

    step

--- tendo21/app/src/main/java/com/example/tendo21/MainActivity.java
+++ tendo21/app/src/main/java/com/example/tendo21/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.tendo21;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.shuhart.stepview.StepView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     StepView step_view;
+    int count = 0;
+    Button next,back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +23,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         step_view = findViewById(R.id.step_view);
+        next = findViewById(R.id.next);
+        back = findViewById(R.id.back);
 
         step_view.setOnStepClickListener(step -> {
-
+            Toast.makeText(MainActivity.this, "Step " + step, Toast.LENGTH_SHORT).show();
         });
+
+        paso();
+
+    }
+
+    public void paso(){
+
+        next.setOnClickListener(v -> {
+            if (count < step_view.getStepCount() -1){
+                count++;
+                step_view.go(count,true);
+            } else {
+                step_view.done(true);
+            }
+        });
+
+        back.setOnClickListener(v -> {
+            if(count > 0){
+                count--;
+            }
+            step_view.done(false);
+            step_view.go(count,true);
+        });
+
+        List<String> numeros = new ArrayList<>();
+        for (int i = 0; i < 5; i++){
+            numeros.add("Numeros " + (i+1));
+        }
+        numeros.set(numeros.size()-1, numeros.get(numeros.size()-1));
+        step_view.setSteps(numeros);
     }
 }
