8c6d98a54c3484332bbe4a622665b0a16821f52b
Tendo456 <rbgamero456@gmail.com>
2021-05-25 10:54:28 -0500

    add

--- tendo21/app/src/main/java/com/example/tendo21/MainActivity.java
+++ tendo21/app/src/main/java/com/example/tendo21/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.shuhart.stepview.StepView;
+
 public class MainActivity extends AppCompatActivity {
+
+    StepView step_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        step_view = findViewById(R.id.step_view);
+
+        step_view.setOnStepClickListener(step -> {
+
+        });
     }
 }
