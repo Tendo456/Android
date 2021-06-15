b31c2086c62ae35e30a144e6d3cf6c441c8d8d9b
Tendo456 <rbgamero456@gmail.com>
2021-06-15 11:34:36 -0500

    aqui

--- labprueba2/app/src/main/java/com/example/labprueba2/UserService.java
+++ labprueba2/app/src/main/java/com/example/labprueba2/UserService.java
@@ -1,9 +1,13 @@
 package com.example.labprueba2;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface UserService {
 
@@ -15,5 +19,8 @@ public interface UserService {
                             @Field("operador") String operador,
                             @Field("dni") String dni,
                             @Field("estado") String estado);
+
+    @GET("Recepcion/ListarRecepcionFecha.php")
+    Call<List<RespPost>> getRecepcionF(@Query("f_inicio") String f_inicio);
 
 }
