08d0b7fd09de161c51cb565051903f7719f5e0f4
Tendo456 <rbgamero456@gmail.com>
2021-07-12 12:19:25 -0500

    carga

---
+++ Quest/app/src/main/java/com/example/quest/carga.java
@@ -0,0 +1,14 @@
+package com.example.quest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class carga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carga);
+    }
+}
