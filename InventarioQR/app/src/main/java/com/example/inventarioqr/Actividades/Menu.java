8a10f2f4aac1d76ec2cb071b8692011c7f657ec7
Tendo456 <rbgamero456@gmail.com>
2022-10-04 07:34:27 -0500

    inventario

---
+++ InventarioQR/app/src/main/java/com/example/inventarioqr/Actividades/Menu.java
@@ -0,0 +1,16 @@
+package com.example.inventarioqr.Actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.inventarioqr.R;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
