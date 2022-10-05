a0a660e53cb64dda5c1c06eb6cc58fd3eb6c9894
Tendo456 <rbgamero456@gmail.com>
2022-10-05 09:21:06 -0500

    qr

---
+++ InventarioQR/app/src/main/java/com/example/inventarioqr/Actividades/Lector.java
@@ -0,0 +1,16 @@
+package com.example.inventarioqr.Actividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.inventarioqr.R;
+
+public class Lector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lector);
+    }
+}
--- InventarioQR/app/src/main/java/com/example/inventarioqr/Actividades/Menu.java
+++ InventarioQR/app/src/main/java/com/example/inventarioqr/Actividades/Menu.java
@@ -2,15 +2,39 @@ package com.example.inventarioqr.Actividades;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.inventarioqr.R;
 
 public class Menu extends AppCompatActivity {
 
+    ImageView scanqr, registrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        scanqr = findViewById(R.id.scanqr);
+        registrar = findViewById(R.id.registrar);
+
+        scanqr.setOnClickListener(v -> scan());
+        registrar.setOnClickListener(v -> registro());
+    }
+
+    public void scan(){
+        Intent intent = new Intent(Menu.this, Lector.class);
+        //intent.putExtra("DisplayUser",nombre.getText().toString());
+        //intent.putExtra("EmailUser",emailuserME);
+        startActivity(intent);
+    }
+
+    public void registro(){
+        Intent intent = new Intent(Menu.this, Lector.class);
+        //intent.putExtra("DisplayUser",nombre.getText().toString());
+        //intent.putExtra("EmailUser",emailuserME);
+        startActivity(intent);
     }
 }
