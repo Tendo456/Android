868623c979295d8b1d10c3184a1846e7e87878fd
Tendo456 <rbgamero456@gmail.com>
2022-12-22 09:03:48 -0500

    format

--- InventarioQR/app/src/main/java/com/example/inventarioqr/RetrofitData/UserService.java
+++ InventarioQR/app/src/main/java/com/example/inventarioqr/RetrofitData/UserService.java
@@ -11,7 +11,6 @@ import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface UserService {
 
@@ -20,24 +19,24 @@ public interface UserService {
 
     @FormUrlEncoded
     @POST("inventarioQR")
-    Call<LectorResponse> insertEquipo (@Field("equipo") String equipo,
-                                       @Field("serie") String serie,
-                                       @Field("marca") String marca,
-                                       @Field("modelo") String modelo,
-                                       @Field("usuario") String usuario,
-                                       @Field("sede") String sede,
-                                       @Field("descripcion") String descripcion);
+    Call<LectorResponse> insertEquipo(@Field("equipo") String equipo,
+                                      @Field("serie") String serie,
+                                      @Field("marca") String marca,
+                                      @Field("modelo") String modelo,
+                                      @Field("usuario") String usuario,
+                                      @Field("sede") String sede,
+                                      @Field("descripcion") String descripcion);
 
     @FormUrlEncoded
     @PUT("inventarioQR/up/{id}")
-    Call<LectorResponse> updateEquipo (@Path("id") String id,
-                                       @Field("equipo") String equipo,
-                                       @Field("serie") String serie,
-                                       @Field("marca") String marca,
-                                       @Field("modelo") String modelo,
-                                       @Field("usuario") String usuario,
-                                       @Field("sede") String sede,
-                                       @Field("descripcion") String descripcion);
+    Call<LectorResponse> updateEquipo(@Path("id") String id,
+                                      @Field("equipo") String equipo,
+                                      @Field("serie") String serie,
+                                      @Field("marca") String marca,
+                                      @Field("modelo") String modelo,
+                                      @Field("usuario") String usuario,
+                                      @Field("sede") String sede,
+                                      @Field("descripcion") String descripcion);
 
     @GET("inventarioQR/getSerie/{serie}")
     Call<List<LectorResponse>> lastReg(@Path("serie") String serie);
